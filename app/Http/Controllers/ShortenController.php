7edc45766196dde69947c94de9263363cc16c2ad
sourovahmad <mdsourovahmad19@gmail.com>
2020-07-04 14:11:30 +0600

    model

---
+++ app/Http/Controllers/ShortenController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\shorten;
+use Illuminate\Http\Request;
+
+class ShortenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\shorten  $shorten
+     * @return \Illuminate\Http\Response
+     */
+    public function show(shorten $shorten)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\shorten  $shorten
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(shorten $shorten)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\shorten  $shorten
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, shorten $shorten)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\shorten  $shorten
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(shorten $shorten)
+    {
+        //
+    }
+}
