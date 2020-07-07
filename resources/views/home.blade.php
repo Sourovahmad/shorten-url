0e0d51656f48a86f0ddaca34ec1d0b76c619a818
sourovahmad <mdsourovahmad19@gmail.com>
2020-07-07 11:15:42 +0600

    bg-image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,27 +12,28 @@
 @endif
 
 
-<div class="container bg-dark container-fluid h-50">
 
-    <div class="row mt-5" >
+<div class="container bg-dark h-50">
+
+    <div class="row mt-5">
 
 
-        <div class="col col-12 text-light ">
+        <div class="col col-12 text-light  mt-5">
 
 
 
 
             <form>
                 <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                   
+                    <label for="exampleInputEmail1">Enter Url</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Url">
+
                 </div>
                 <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <label for="exampleInputPassword1">custom Url</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="custom Url">
                 </div>
-            
+
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
 
@@ -48,40 +49,6 @@
 
 </div>
 
-
-
-
-<!-- <section class="full-width-content text-content url-shorten-row" style="background-image:url();background-color:#0b1736;height: 122px;
-     margin-top: 155;">
-    <div class="grid-container">
-        <div class="inner-content grid-x grid-margin-x grid-padding-x">
-            <div class="cell">
-                <div id="form_container" class="t-center mid-container foot-room">
-
-                    <form class="form-inline" method="post" action="{{route('urlstore')}}">
-                        @csrf
-                        <div class="form-group" style="height: 122px; margin-left:204px; padding: 29px">
-                            <label for="exampleInputEmail1">Enter your URL :</label>
-                            <input type="text" class="form-control" id="url" name="url" placeholder="Enter url">
-
-                        </div>
-
-                        <div class="form-group">
-                            <label for="exampleInputPassword1">Shorten Url :</label>
-                            <input type="text" class="form-control" name="shorten" placeholder="enter short url">
-                        </div>
-
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-
-
-                </div>
-            </div>
-        </div>
-    </div>
-  
-</section>
- -->
 
 
 
